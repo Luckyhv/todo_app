5898a8acfd2fc0687888bf166c57d01c5de4c07a
Luckyhv <harshavardhan9680@gmail.com>
2022-03-04 22:52:40 +0530

    Enter

---
+++ src/Components/Textform.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import './Textform.css';
+import { useState } from 'react';
+// import {IoClose } from 'react-icons/io5';
+
+export default function Textform(props) {
+    const [text, setText] = useState("");
+    function handlechange(event) {
+        setText(event.target.value);
+    }
+    const [item, setItem] = useState([]);
+    function clickbtn() {
+        if (text !== "") {
+            setItem([...item, text]);
+        }
+    }
+    const deleteitem = (id) => {
+        const updatedItems = item.filter((element, ind) => {
+            return ind !== id;
+        })
+        setItem(updatedItems);
+    }
+    return (
+        <>
+        <div className="container">
+            <div className='mid'><h2>Todo List</h2>
+                <form id="addForm">
+                    <div className="row">
+                        <div className="col-lg-7 col-md-7 col-sm-7">
+                            <input type="text"
+                                className="form-control" id="item" value={text} placeholder="Enter Task" onChange={handlechange} />
+                        </div>
+                        <div className="col-lg-5 col-md-5 col-sm-5">
+                            <button type="button" className="btn btn-primary add" onClick={clickbtn} >Add</button>
+                        </div>
+                        <div>
+                            <h2 style={{ margin: "15px 0px" }}>Tasks</h2>
+                            {item.map((element, ind) => {
+                                return (
+                                    <div className="eachItem " key={ind}>
+                                        <h4>{element}</h4>
+                                        <i className="far fa-trash-alt add-btn" title="Delete Item" onClick={() => deleteitem(ind)}></i>
+                                    </div>
+                                )
+                            })
+                        }
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        </>
+    )
+}
