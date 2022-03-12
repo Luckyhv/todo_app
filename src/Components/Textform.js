36ad1c576c2e529149db68fd01e9c0cef26ab2bf
Luckyhv <harshavardhan9680@gmail.com>
2022-03-12 09:28:16 +0530

    Enter

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Textform.css';
 import { useState } from 'react';
 // import {IoClose } from 'react-icons/io5';
 
 export default function Textform(props) {
+    const getitemss=()=>{
+        let list=localStorage.getItem('list');
+        if(list){
+            return JSON.parse(localStorage.getItem('list'));
+        }
+        else{
+            return [];
+        }
+    }
     const [text, setText] = useState("");
     function handlechange(event) {
         setText(event.target.value);
     }
-    const [item, setItem] = useState([]);
+    const [item, setItem] = useState(getitemss);
     function clickbtn() {
         if (text !== "") {
             setItem([...item, text]);
+            setText("");
         }
     }
     const deleteitem = (id) => {
@@ -20,6 +30,11 @@ export default function Textform(props) {
         })
         setItem(updatedItems);
     }
+
+    useEffect(()=>{
+        localStorage.setItem('list',JSON.stringify(item));
+    },[item])
+
     return (
         <>
         <div className="container">
@@ -28,7 +43,7 @@ export default function Textform(props) {
                     <div className="row">
                         <div className="col-lg-7 col-md-7 col-sm-7">
                             <input type="text"
-                                className="form-control" id="item" value={text} placeholder="Enter Task" onChange={handlechange} />
+                                className="form-control" id="item" autocomplete="off" value={text} placeholder="Enter Task" onChange={handlechange} />
                         </div>
                         <div className="col-lg-5 col-md-5 col-sm-5">
                             <button type="button" className="btn btn-primary add" onClick={clickbtn} >Add</button>
